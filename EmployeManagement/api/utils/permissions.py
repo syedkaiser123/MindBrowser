c2fed0bd5e77ac59b30192cfc7567c96fb4ef50b
syedkaiser123 <44358004+syedkaiser123@users.noreply.github.com>
2021-02-25 06:48:04 +0000

    Delete EmployeManagement/api directory

--- EmployeManagement/api/utils/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission, DjangoObjectPermissions
-from django.contrib.auth.models import Group, Permission
-
-class AccountsManagementPermission(BasePermission):
-
-    def has_permission(self, request, view):
-        if request.user.is_superuser or request.user.groups.filter(name='Manager').exists():
-            return True
-        return False
